5088cd0be2b8fc867099017da4d3ae23bb353d38
Kyle Kelley <rgbkrk@gmail.com>
2018-06-15 16:11:07 -0700

    store events in rethinkdb for now

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-module.exports = robot => {
-  // Your code here
-  robot.log('Yay, the robot was loaded!')
+const r = require("rethinkdb");
 
-  robot.on(['check_suite', 'pull_request'], async context => {
-    robot.log({event: context.event})
-  })
+const DATABASE_NAME = "test";
+const TABLE_NAME = "galleria_github_events";
+
+async function makeBot(robot) {
+  const connection = await r.connect({ host: "localhost", port: 28015 });
+  const table = await r.db(DATABASE_NAME).tableCreate(TABLE_NAME);
+
+  console.log(table);
+
+  robot.log("Galleria is ready to go 🖼Y!");
+
+  robot.on(["check_run", "check_suite", "pull_request"], async context => {
+    const well = await r.table(TABLE_NAME).insert([context.payload]);
+
+    console.log("soooooo well then");
+    console.log(well);
+
+    robot.log({ event: context.event });
+    robot.log({ action: context.payload.action });
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
@@ -12,3 +27,4 @@ module.exports = robot => {
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 }
+module.exports = makeBot;
